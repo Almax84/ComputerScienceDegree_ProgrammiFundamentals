1ca194676bff665c6f69935ef42ec2317a7fe896
davscr <davide.scrimieri@assistdigital.com>
2018-10-18 21:19:24 +0200

    passing es1 hw 1

--- homework2018/homework01/program01.py
+++ homework2018/homework01/program01.py
@@ -14,6 +14,31 @@ ATTENZIONE: la lista ls dei voti al termine della funzione NON DEVE risultare mo
 Ad esempio per voti=[7,5,8,3,7,2,9] la funzione es2 restituisce la lista
 [7, 7, 7, 6, 5, 5, 4, 4, 2, 1]
 
+i = 
+4
+
+voti = [2,3,5,7,7,8,9]
+i = 0
+7
+i = 1
+7
+i = 2
+7
+i = 3
+6
+i = 4
+5
+i = 5
+5
+i = 6
+4
+i = 7
+4
+i = 8
+2
+i = 9
+1
+
 NOTA: il timeout previsto per questo esercizio è di 1 secondo per ciascun test
 
 ATTENZIONE: quando caricate il file assicuratevi che sia nella codifica UTF8 (ad esempio editatelo dentro Spyder)
@@ -23,17 +48,23 @@ def es1(voti):
     sorted_voti = sorted(voti, reverse = False)
 
     return_list = []
-    
     i = 0
     C = sorted_voti[len(sorted_voti)-1]
+    # i ->  voto di riferimento, nonché indice della lista di ritorno
+    # voto -> voto singolo studente
     while i < C+1:
 
         contatore_studenti_ammessi = 0
         min_value = 0
         for j in range(len(sorted_voti)):
-            vt = sorted_voti[j]
-            if vt >= i:
+            voto = sorted_voti[j]
+            if voto > i:
                 contatore_studenti_ammessi+=1
+            elif voto == i:
+                # se il voto è esattamente uguale all'indice, essendo la lista ordinata, gli studenti ammessi saranno uguali alla lunghezza della sotto-lista a partire dall'indice dove si trova i
+                
+                contatore_studenti_ammessi = len(sorted_voti[j:])
+                break
             else:
                 min_value = j
         sorted_voti = sorted_voti[min_value:]
