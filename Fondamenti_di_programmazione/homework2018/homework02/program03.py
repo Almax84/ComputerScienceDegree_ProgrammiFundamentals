deb313a68c7f16de2b2e81201d649e7dc82a3689
davscr <davide.scrimieri@hotmail.it>
2018-11-17 23:38:35 +0100

    update hw2 test and program03 EUREKA!

--- Fondamenti_di_programmazione/homework2018/homework02/program03.py
+++ Fondamenti_di_programmazione/homework2018/homework02/program03.py
@@ -6,12 +6,13 @@ def es3(fmappa):
         p2 = []
         insieme_I = dict()
 
-        p1 = get_percorso_robottino(file_lines, p1)
-        p2 = get_percorso_robottino(file_lines, p2)
-
         tuples_string = ''.join(file_lines).strip()
 
         get_tuple_dict(insieme_I, tuples_string)
+
+        p1 = get_percorso_robottino(file_lines, p1)
+        p2 = get_percorso_robottino(file_lines, p2)
+
 
         spostamento_p1 = get_spostamento_robottino(p1)
         spostamento_p2 = get_spostamento_robottino(p2)
@@ -21,16 +22,25 @@ def es3(fmappa):
         for x, rows in insieme_I.items():
             for y in rows:
                 if x not in spostamento_p1 or x not in spostamento_p2:
+                    break
+
+                y2_list = spostamento_p2[x]
+                y1_list = spostamento_p1[x]
+
+                if y in y2_list or y in y1_list:
                     continue
 
-                y2_list = spostamento_p1[x]
-                y1_list = spostamento_p2[x]
+                #eventualmente eliminare questi due controlli
+                y1_minori = list(filter(lambda y1: y1 < y, y1_list))
+                y2_maggiori = list(filter(lambda y2: y2 > y, y2_list))
 
-                for y2 in y2_list:
-                    if y < y2:
-                        for y1 in y1_list:
-                            if y > y1:
-                                contatore_circoscritti+=1
+                y1_maggiori = list(filter(lambda y1: y1 > y, y1_list))
+                y2_minori = list(filter(lambda y2: y2 < y, y2_list))
+
+                if (len(y1_minori)>0 and len(y2_maggiori)>0) or (len(y1_maggiori)>0 and len(y2_minori)>0):
+                    contatore_circoscritti+=1
+
+
 
         return contatore_circoscritti
 
@@ -39,7 +49,7 @@ def es3(fmappa):
 def get_spostamento_robottino(percorso_primo_robottino):
     x = 1
     y = 1
-    mappa_spostamenti_robottino = dict()  # key will be column x, value list of y
+    mappa_spostamenti_robottino = {1:[1]}  # key will be column x, value list of y
     for spostamento in percorso_primo_robottino:
         if spostamento > 0:
             for i in range(1, spostamento + 1):
@@ -94,5 +104,3 @@ def get_percorso_robottino(file_lines, percorso_rotottino):
         i += 1
     return percorso_rotottino
 
-
-#print(es3("mp2.txt"))
--- Fondamenti_di_programmazione/homework2018/homework02/test_03.py
+++ Fondamenti_di_programmazione/homework2018/homework02/test_03.py
@@ -21,37 +21,12 @@ class Test(testlib.TestCase):
         self.check(result,       expected, None, "il risultato non e' corretto")
         return 1
 
-    def test_mp1(self):
-        '''...'''
-        return self.do_test('mp1.txt', 4)
-
-    def test_mp2(self):
-        '''istanza in cui la zona circoscritta e' un rettangolo di base 10000 e altezza 20
-        e l'insieme contiene 9000 quadrati interni equispaziati ad altezza 5 e 10 e 15
-        e 1000 quadrati esterni equispaziati ad altezza 25 '''
-        return self.do_test('mp2.txt', 3000)
-        
-    def test_mp3(self):
-        '''istanza in cui 
-        -il primo cammino e' formato da una un'alternanza di 2 passi 
-        in alto e due passi a destra per 5000 volte. 
-        -Il secondo cammino parte con 4 passi a destra e poi ha un alternanza di due passi 
-        in alto e due a destra per 5000-2 volte e 4 passi in alto per finire.
-        L'insieme contiene 3000 quadrati (x,x-4),(x,x),(x,x+4) dove x e' qualunque multiplo di 10
-        fino a 10000.
-        '''
-        return self.do_test('mp3.txt', 1000)
-        
-    def test_mp4(self):
-        '''istanza in cui 
-        la zona circoscritta e' un rettangolo di base 4 e altezza 100000
-        l'insieme dei quadrati e' composto da quadrati del tipo (3,x) e (5,x) e (8,x)
-        con x multiplo di 1000 e non superiore a 100000
-        '''
-        return self.do_test('mp4.txt', 200)
-
-
-
+    @file_data('test_03.json')
+    def test_from_json(self, enabled, filename, expected, descrizione):
+        if enabled:
+            return self.do_test(filename, expected)
+        else:
+            raise unittest.SkipTest("Test disabled")
 
 if __name__ == '__main__':
     Test.main()
