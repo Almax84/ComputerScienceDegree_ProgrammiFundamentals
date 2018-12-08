d5fc39ec50ce94965cdba4058487facf2a6d7f03
davscr <davide.scrimieri@hotmail.it>
2018-12-08 12:04:16 +0100

    minor fixes hw3

--- Fondamenti_di_programmazione/homework2018/homework03/program01.py
+++ Fondamenti_di_programmazione/homework2018/homework03/program01.py
@@ -1,7 +1,6 @@
 import immagini
 
-#THIS IS CURRENTLY THE COMMITTED FILE!
-#TODO RE-SUBMIT FILE WITH RENAMED VARIABLES!
+
 
 def es1(fimg, fimg1):
     image = immagini.load(fimg)
@@ -13,29 +12,29 @@ def es1(fimg, fimg1):
         c = 0
         image_row = image[r]
         while c < altezza:
-            if c + 3 < lunghezza and isBlack(image_row[c]) and isBlack(image_row[c + 3]):
+            if c + 3 < lunghezza and is_black(image_row[c]) and is_black(image_row[c + 3]):
                 c += 4
                 continue
 
             currentPixel = image_row[c]
-            if (isWhite(currentPixel)):
+            if (is_white(currentPixel)):
                 rect_corners = []
                 can_start_rect_from_here(r, c, image, lunghezza, altezza, final_solution, rect_corners)
                 if not len(rect_corners) == 0:
                     c = rect_corners[0][1]
                     continue
 
-            c = c + 1
+            c +=1
 
     for rect in final_solution:
-        writeGreen(rect, image)
-        writeRed(rect, image)
+        write_green(rect, image)
+        write_red(rect, image)
 
     immagini.save(image, fimg1)
     return len(final_solution)
 
 
-def writeGreen(rect, image):
+def write_green(rect, image):
     left_up_corner = rect[0]
     right_up_corner = rect[1]
     left_down_corner = rect[2]
@@ -45,15 +44,13 @@ def writeGreen(rect, image):
 
     # lato sopra
     left_up_corner_x = left_up_corner[0]
-    for c in range(left_up_corner[1], right_up_corner[1] + 1):
-        image[left_up_corner_x][c] = GREEN
+    write_lato_sotto(GREEN, image, left_up_corner_x, left_up_corner[1], right_up_corner[1])
 
     # lato sotto
     left_down_corner_x = left_down_corner[0]
     left_downn_corner_y = left_down_corner[1]
     right_down_corner_y = right_downd_corner[1]
-    for c in range(left_downn_corner_y, right_down_corner_y + 1):
-        image[left_down_corner_x][c] = GREEN
+    write_lato_sotto(GREEN, image, left_down_corner_x, left_downn_corner_y, right_down_corner_y)
 
     # LATO SINISTRO
     for r in range(left_up_corner_x, left_down_corner_x + 1):
@@ -64,7 +61,12 @@ def writeGreen(rect, image):
         image[r][right_up_corner[1]] = GREEN
 
 
-def writeRed(rect, image):
+def write_lato_sotto(GREEN, image, left_down_corner_x, left_downn_corner_y, right_down_corner_y):
+    for c in range(left_downn_corner_y, right_down_corner_y + 1):
+        image[left_down_corner_x][c] = GREEN
+
+
+def write_red(rect, image):
     RED = (255, 0, 0)
     left_up_corner = rect[0]
     right_up_corner = rect[1]
@@ -94,20 +96,20 @@ def can_start_rect_from_here(r, c, image, lunghezza, altezza, final_solution, re
         # se nero o alla fine dell'immagine allora OK
         # Se e l'ultima colonna verifico se e' bianco o nero
         if r == (lunghezza - 1):
-            if isBlack(current_pixel):
+            if is_black(current_pixel):
                 # se all'ultima colonna troviamo nero e ancora non abbiamo individuato un angolo allora non e' un rettangolo
                 return False
-            elif isWhite(current_pixel):
+            elif is_white(current_pixel):
                 # questa colonna e' l'ultima
                 right_up_corner = (left_up_corner_x, r)
                 rect_corners.append(right_up_corner)
                 return False
         # se e' nero e al passo precedente non abbiamo trovato un angolo allora non puo' essere un angolo
-        if isBlack(current_pixel):
+        if is_black(current_pixel):
             return False
         # se il pixel nell'ultima colonna e' bianco quindi la riga continua verifichiamo se sotto e' bianco in tal caso e' un angolo in alto a destra e ci fermiamo
         # (left_up_corner[0] + 1) < altezza controlla che sotto sia presente una riga
-        elif isWhite(current_pixel) and (left_up_corner_x + 1) < altezza and isWhite(image[left_up_corner_x + 1][r]):
+        elif is_white(current_pixel) and (left_up_corner_x + 1) < altezza and is_white(image[left_up_corner_x + 1][r]):
             right_up_corner = (left_up_corner_x, r)
             rect_corners.append(right_up_corner)
             break
@@ -132,10 +134,10 @@ def can_start_rect_from_here(r, c, image, lunghezza, altezza, final_solution, re
 
         if r == (altezza - 1):
             # se proprio l ultima riga e' bianca almeno negli inner allora puo essere un rettangolo
-            if isWhite(left_pixel) and isWhite(right_pixel):
+            if is_white(left_pixel) and is_white(right_pixel):
                 left_pixel_inner = image[r][left_up_corner_y + 1]
                 right_pixel_inner = image[r][left_up_corner_y - 1]
-                if isWhite(left_pixel_inner) and isWhite(right_pixel_inner):
+                if is_white(left_pixel_inner) and is_white(right_pixel_inner):
                     # e' una possibile riga bianca quindi e' la base del rett
                     left_down_corner = (r, left_up_corner_y)
                     right_down_corner = (r, right_up_corner_y)
@@ -145,57 +147,47 @@ def can_start_rect_from_here(r, c, image, lunghezza, altezza, final_solution, re
             else:
                 # se non sono entrambi bianchi e siamo arrivati alla fine allora non e' un rettangolo, manca la base
                 return False
-        if isWhite(left_pixel) and isWhite(right_pixel):
+        if is_white(left_pixel) and is_white(right_pixel):
             # se sono entrambi bianchi allora va bene, controlliamo solo che non sia una potenziale riga bianca
             left_pixel_inner = image[r][left_up_corner_y + 1]
             right_pixel_inner = image[r][right_up_corner_y - 1]
-            if isWhite(left_pixel_inner) and isWhite(right_pixel_inner):
+            if is_white(left_pixel_inner) and is_white(right_pixel_inner):
                 # e' una possibile riga bianca quindi e' la base del rett
                 left_down_corner = (r, left_up_corner_y)
                 right_down_corner = (r, right_up_corner_y)
                 break
-            elif isBlack(left_pixel_inner) and isBlack(right_pixel_inner):
+            elif is_black(left_pixel_inner) and is_black(right_pixel_inner):
                 # se gli inner sono entrambi neri allora dobbiamo proseguire, prima pero' controlliamo che la riga sia nera
                 if not rectLength == min_length:
                     for ir in range(left_up_corner_y + 2, right_up_corner_y - 1):
-                        if isWhite(image[r][ir]):
+                        if is_white(image[r][ir]):
                             return False
                         else:
                             continue
             else:
-                # se sono uno bianco e uno nero allora non va bene
                 return False
         else:
-            # se sono entrambi neri o uno bianco e uno nero allora non puo' essere un rettangolo
             return False
 
-    # if left_up_corner == -1 or right_up_corner == -1 or left_down_corner == -1 or right_down_corner == -1:
-    #     return False
 
     if left_down_corner[0] - left_up_corner_x + 1 < min_length:
-        # print("Rettangolo troppo poco alto")
         return False
 
-    # controlliamo la base sia interamente bianca
     for i in range(left_down_corner[1], right_down_corner[1] + 1):
         current_pixel = image[left_down_corner[0]][i]
-        if isBlack(current_pixel):
+        if is_black(current_pixel):
             return False
 
-    # print("RETTANGOLO!!!")
     final_solution.append([left_up_corner, right_up_corner, left_down_corner, right_down_corner])
     return True
 
 
-def isBlack(tupla):
+def is_black(tupla):
     BLACK = (0, 0, 0)
     return tupla == BLACK
 
 
-def isWhite(tupla):
+def is_white(tupla):
     WHITE = (255, 255, 255)
     return tupla == WHITE
 
-
-if __name__ == '__main__':
-    es1('e1_f5.png', 'test5.png');
--- Fondamenti_di_programmazione/homework2018/homework03/program02.py
+++ Fondamenti_di_programmazione/homework2018/homework03/program02.py
@@ -1,67 +1,3 @@
-# Possiamo rappresentare lo skyline di una citta' con un numero di rettangoli di 
-# diversi colori e dimensioni su di uno sfondo omogeneo. Vedi ad esempio i file es2_risTest*.png .
-# Uno skyline e' dunque una sequenza di rettangoli posizionati sull'asse x delle ascisse. 
-# La posizione del rettangolo all'interno dello skyline (nel seguito posizione del rettangolo) 
-# e' individuata univocamente dalla coordinata x occupata dal suo vertice in basso a sinistra.
-# Uno stesso rettangolo puo' essere presente piu' volte all'interno della sequenza in diverse posizioni. 
-# 
-# Per i nostri skyline valgono i seguenti vincoli:
-# 1) nello skyline non compaiono mai due rettangoli con la stessa posizione.
-# 2) nello skyline non compare mai un rettangolo che ha lo stesso colore dello sfondo.
-# 3) Se due rettangoli si intersecano, quello che ha luminosita' massima appare in primo piano e 
-# in caso di pari luminosita'  e' in primo piano il rettangolo posizionato piu' a sinistra 
-# (la luminosita' di un rettangolo e' la somma delle tre componenti  del suo colore)
-#  
-# Definire una classe Colore, una classe Rettangolo e una classe Skyline secondo le seguenti specifiche.
-# 
-# La classe Colore deve implementare i seguenti metodi:
-# - __init__(self,r=0,g=0,b=0)  che inizializza un colore con la terna RGB (r,g,b) valida.
-# - utilizzo(self, sk) dove sk e' un oggetto di tipo Skyline. Il metodo ritorna  il numero di occorrenze 
-#   di rettangoli con il colore self presenti nello skyline sk 
-#   (ricorda che in uno skyline uno stesso rettangolo puo' comparire piu' volte in diverse posizioni)
-# - to_tuple(self) che torna la terna (r, g, b)
-# 
-# La classe Rettangolo deve implementare i seguenti metodi:
-# - __init__(base, altezza, colore) Base ed altezza sono due interi positivi e 
-#   rappresentano la lunghezza della base e dell'altezza del rettangolo, colore e' un oggetto 
-#   della classe Colore. 
-# - cancella(self) cancella le occorrenze del rettangolo da tutti gli skyline in cui e' presente. 
-# - to_tuple(self) che torna la terna (base, altezza, colore)
-# 
-# La classe Skyline deve implementare i seguenti metodi:
-# - __init__(self, sfondo) dove sfondo e' un oggetto di tipo Colore. 
-#   definisce uno skyline vuoto con colore di sfondo uguale a 'sfondo'. 
-# - aggiungi(self, ret, x) l' oggetto ret di tipo Rettangolo viene aggiunto  
-#   allo skyline a partire dall'ascissa x, l'aggiunta avviene solo se non vengono violate le regole 1), 2) e 3) 
-#   dello skyline. 
-# - fondi(self, other) con argomento other di tipo Skyline, che inserisce nello skyline self tutte le occorrenze  
-#   di rettangoli dello skyline other. I rettangoli vanno inseriti nelle stesse posizioni che occupavano in other 
-#   e l'inserimento di ciascun rettangolo avviene solo se non viola le regole degli skyline 
-#   (ricorda che in uno skyline non e' possibile inserire rettangoli che hanno lo stesso colore dello sfondo 
-#   ne' rettangoli da posizionare ad una ascissa gia' occupata).
-# - salva(self, fimg)   salva  l'immagine dello skyline sotto forma di file PNG all'indirizzo fimg. 
-# - larghezza(self) restituisce la larghezza dello skyline (vale a dire il valore massimo di x+base, 
-#   dove base e' la base del rettangolo inserito alla posizione x).
-#   Uno skyline vuoto ha per convenzione larghezza zero.
-# - altezza(self) restituisce l'altezza dello skyline (vale a dire l'altezza massima tra quelle dei 
-#   rettangoli presenti nello skyline). Uno skyline vuoto ha per convenzione altezza zero.
-# - edifici(self) restituisce il numero di rettangoli presenti nello skyline.
-# - to_tuple(self) che torna la tupla (sfondo,)
-# 
-# DEFINIZIONE DELLE CLASSI
-# Siete liberi di scegliere sia gli attributi da usare in ciascun oggetto che la loro implementazione.
-# Se lo ritenete utile potete aggiungere altri metodi alle vostre classi.
-# 
-# GESTIONE DEGLI ERRORI
-# Tutti i metodi ed i costruttori devono controllare che gli argomenti forniti siano corretti, 
-# 
-# Per  salvare i file PNG si possono usare la funzione  save della libreria immagini.
-# 
-# NOTA: il timeout previsto per questo esercizio e' di 1 secondo per ciascun test.
-# 
-# ATTENZIONE: non potete usare altre librerie.
-# 
-# ATTENZIONE: quando consegnate il programma assicuratevi che sia nella codifica UTF8   
 
 import immagini 
 
@@ -73,80 +9,40 @@ class Colore:
     def __init__(self, r=0, g=0, b=0):
         if not Colore.isColorValid(r) or not Colore.isColorValid(g) or not Colore.isColorValid(b):
             raise ValueError('Colore non valido')
-        self.r = r;
-        self.g = g;
-        self.b = b;
+        self.r = r
+        self.g = g
+        self.b = b
 
     def utilizzo(self, sk):
         if not isinstance(sk, Skyline):
             raise ValueError("Skylines non valido")
-        counter = 0;
-        #if sk.toDraw == True:
-            #sk.disegna()
+        counter = 0
 
-        for position, rect in sk.rectMap.items():
+
+        for position, rect in sk.rect_map.items():
             if self == rect.colore:
-                counter = counter + 1
-                # per ogni rettangolo che ha lo stesso colore verifichiamo se e' stato disegnato
-#                 found = False
-#                 # vediamo se il rettangolo e' stato disegnato effettivamente
-#                 for row in range(int(position), int(position) + rect.base):
-#                         for col in range(rect.altezza):
-#                                 if sk.imsAsRectPos[row][col] == int(position):
-#                                     found = True
-#                                     break
-#                         if found == True:
-#                             break
-#                 if found == True:
-#                     counter = counter + 1
-                        
-#         for position, rect in sk.rectMap.items():
-            
-#             if self == rect.colore:
-#                 leftDownCorner = (int(position), 0)
-#                 rightDownCorner = (int(position) + rect.base, 0)
-#                 leftUpCorner = (leftDownCorner[0], rect.altezza)
-#                 rightUpCorner = (rightDownCorner[0], rect.altezza)
-#                 willBePainted = True
-#                 # check if current rect will be painted
-#                 for position2, rect2 in sk.rectMap.items():
-#                     if int(position) != int(position2) and rect.brightness() <= rect2.brightness():
-#                         leftDownCorner2 = (int(position2), 0)
-#                         rightDownCorner2 = (int(position2) + rect2.base, 0)
-#                         leftUpCorner2 = (leftDownCorner2[0], rect2.altezza)
-#                         rightUpCorner2 = (rightDownCorner2[0], rect2.altezza)
-#                         if(leftDownCorner[0] > leftDownCorner2[0] and rightDownCorner[0] <= rightDownCorner2[0] and leftUpCorner[1] <= leftUpCorner2[1] and rightUpCorner[1] <= rightUpCorner2[1]):
-#                             willBePainted = False
-#                             break
-#                 if willBePainted == True:
-#                     counter = counter + 1
+                counter +=  1
         return counter
 
     def to_tuple(self):
         return (self.r, self.g, self.b)
-    
-    def __eq__(self, other):
-        if not self.to_tuple() == other.to_tuple():
-            return False
-        else:
-            return True
 
     def brightness(self):
         return self.r + self.g + self.b
     
-    @staticmethod
+    @staticmethod #because i'm a java nostalgic
     def static_brightness(tupla):
         return tupla[0] + tupla[1] + tupla[2]
         
-    @staticmethod
-    def isColorValid(rgbColor):
+    @staticmethod #just for funzies
+    def isColorValid(rgb_color):
         try:
-            if not isinstance(rgbColor, int):
+            if not isinstance(rgb_color, int):
                 return False
         except ValueError:
             return False
            
-        if rgbColor < 0 or rgbColor > 255:
+        if rgb_color < 0 or rgb_color > 255:
             return False;
         else:
             return True
@@ -170,40 +66,35 @@ class Rettangolo:
         
         if not isinstance(colore, Colore):
             raise ValueError("Sfondo Rettangolo non valido")
-        self.base = base;
-        self.altezza = altezza;
-        self.colore = colore;
-        self.skylineRef = []
+        self.base = base
+        self.altezza = altezza
+        self.colore = colore
+        self.skyline_ref = []
 
     def cancella(self):
-        for ref in self.skylineRef:
-            localSkyline = ref[0]
-            localPosition = ref[1]
-            if localPosition in localSkyline.rectMap:
-                del localSkyline.rectMap[localPosition]
-                localSkyline.toDraw = True
-                maxWidth = 0
-                maxHeight = 0
-                for position, rect in localSkyline.rectMap.items():
-                    if (position + rect.base) > maxWidth:
-                        maxWidth = position + rect.base
-                    if rect.altezza > maxHeight:
-                        maxHeight = rect.altezza
-                localSkyline.width = maxWidth
-                localSkyline.height = maxHeight
-        self.skylineRef = []
+        for ref in self.skyline_ref:
+            local_skyline = ref[0]
+            local_position = ref[1]
+            if local_position in local_skyline.rect_map:
+                del local_skyline.rect_map[local_position]
+                local_skyline.to_draw = True
+                max_width = 0
+                max_height = 0
+                #calcolo larghezza ed altezza del nuovo skyline
+                for position, rect in local_skyline.rect_map.items():
+                    if (position + rect.base) > max_width:
+                        max_width = position + rect.base
+                    if rect.altezza > max_height:
+                        max_height = rect.altezza
+                local_skyline.width = max_width
+                local_skyline.height = max_height
+        self.skyline_ref = []
         
     def brightness(self):
         return self.colore.brightness();
 
-    def __eq__(self, other):
-        if not self.to_tuple() == other.to_tuple():
-            return False
-        else:
-            return True
-        
-    def aggiungiSkylineRef(self, skyline, position):
-        self.skylineRef.append((skyline, position))
+    def aggiungi_skyline_ref(self, skyline, position):
+        self.skyline_ref.append((skyline, position))
         
     def to_tuple(self):
         return (self.base, self.altezza, self.colore);
@@ -217,11 +108,11 @@ class Skyline:
         if not isinstance(sfondo, Colore):
             raise ValueError("Sfondo Skyline non valido")
         self.sfondo = sfondo
-        self.toDraw = True
+        self.to_draw = True
         self.width = 0
         self.height = 0
-        self.imgAsPixelArray = []
-        self.rectMap = {}  # dizionario di rettangoli presenti nello skyline, key: posizione (e' univoca) value: rettangolo
+        self.img_as_pixesl_array = []
+        self.rect_map = {}  # dizionario di rettangoli presenti nello skyline, key: posizione (e' univoca) value: rettangolo
 
     def aggiungi(self, rettangolo, x):
         if not isinstance(rettangolo, Rettangolo):
@@ -233,14 +124,12 @@ class Skyline:
         
         if self.sfondo == rettangolo.colore:
             return
-            #raise ValueError('Rettangolo non valido, stesso sfondo dello skyline')
-        
-        if x in self.rectMap:
+
+        if x in self.rect_map:
             return
-            #raise ValueError('Rettangolo gia presente nella stessa posizione')
-        
+
         # aggiungo il rettangolo
-        self.rectMap[x] = rettangolo
+        self.rect_map[x] = rettangolo
         
         # ricalcolo massima lunghezza e altezza
         if (x + rettangolo.base) > self.width:
@@ -248,19 +137,18 @@ class Skyline:
         if rettangolo.altezza > self.height:
             self.height = rettangolo.altezza
         # aggiungo reference
-        rettangolo.aggiungiSkylineRef(self, x)
-        self.toDraw = True
+        rettangolo.aggiungi_skyline_ref(self, x)
+        self.to_draw = True
 
     def fondi(self, other):
         if not isinstance(other, Skyline):
             raise ValueError("Skyline non valido")
-        for position, rect in other.rectMap.items():
+        for position, rect in other.rect_map.items():
             try:
-                #newRect = Rettangolo(rect.base, rect.altezza, rect.colore)
                 self.aggiungi(rect, position)
             except:
-                print('Go head')
-        self.toDraw = True
+                pass
+        self.to_draw = True
 
     def salva(self, fimg):
         if not isinstance(fimg, str):
@@ -268,41 +156,32 @@ class Skyline:
         if not (fimg.endswith('.png')  or fimg.endswith('.PNG')):
             raise ValueError("FileImg is not a valid file name")
         
-        if self.toDraw == True:
+        if self.to_draw == True:
             self.disegna()
         
-        immagini.save(self.imgAsPixelArray, fimg)
-        #print("Immagine Salvata!")
+        immagini.save(self.img_as_pixesl_array, fimg)
 
     def disegna(self):
         lst = [None] * self.width
-        #lstPosition = [-1] * self.width
         for i in range (self.width):
             lst[i] = [self.sfondo.to_tuple()] * self.height
-            #lstPosition[i] = [-1] * self.height
             
-        self.imgAsPixelArray = lst
-        #self.imsAsRectPos = lstPosition
+        self.img_as_pixesl_array = lst
         # scorro i rettangoli da inserire in ordine di posizione, cosi se trovo stessa brightness comunque non lo inserisco
-        for position, rect in sorted(self.rectMap.items()):
+        for position, rect in sorted(self.rect_map.items()):
             for row in range(position, position + rect.base):
                 for col in range(rect.altezza):
-#                     if row == 80:
-#                         print('eccomi')
-                    if self.imgAsPixelArray[row][col] == self.sfondo.to_tuple():
-                        self.imgAsPixelArray[row][col] = rect.colore.to_tuple()
-                        #self.imsAsRectPos[row][col] = position
-                    elif rect.colore.brightness() > Colore.static_brightness(self.imgAsPixelArray[row][col]):
-                        #print('sovrascrivo ' + row + ' ' + col + ' con ' + rect.colore.to_tuple())
-                        self.imgAsPixelArray[row][col] = rect.colore.to_tuple()
-                        #self.imsAsRectPos[row][col] = position
-                        
+                    if self.img_as_pixesl_array[row][col] == self.sfondo.to_tuple():
+                        self.img_as_pixesl_array[row][col] = rect.colore.to_tuple()
+                    elif rect.colore.brightness() > Colore.static_brightness(self.img_as_pixesl_array[row][col]):
+                        self.img_as_pixesl_array[row][col] = rect.colore.to_tuple()
+
         # rotate 3 times
-        self.imgAsPixelArray = list(zip(*self.imgAsPixelArray[::-1]))
-        self.imgAsPixelArray = list(zip(*self.imgAsPixelArray[::-1]))
-        self.imgAsPixelArray = list(zip(*self.imgAsPixelArray[::-1]))
+        self.img_as_pixesl_array = list(zip(*self.img_as_pixesl_array[::-1]))
+        self.img_as_pixesl_array = list(zip(*self.img_as_pixesl_array[::-1]))
+        self.img_as_pixesl_array = list(zip(*self.img_as_pixesl_array[::-1]))
         
-        self.toDraw = False
+        self.to_draw = False
         
     def larghezza(self):
         return self.width
@@ -311,52 +190,10 @@ class Skyline:
         return self.height
 
     def edifici(self):
-        return len(self.rectMap)
+        return len(self.rect_map)
 
     def to_tuple(self):
         return (self.sfondo,)
 
 
 # ################################################################################
-BLACK = (0, 0, 0);
-WHITE = (255, 255, 255);
-GREEN = (0, 255, 0);
-RED = (255, 0, 0);
- 
-if __name__ == '__main__':
-    green = Colore(0, 255, 0)
-    black = Colore(0, 0, 0);
-    red = Colore(255, 0, 0)
-    white = Colore(255, 255, 255)
-    greenRect = Rettangolo(70, 90, green)
-    redRect = Rettangolo(50, 50, red)
-    whiteRect = Rettangolo(60, 40, white)
-    blackRect = Rettangolo(60, 120, black)
-    sk = Skyline(black)
-#     sk.aggiungi(whiteRect, 10)
-#     sk.aggiungi(whiteRect, 60)
-#     sk.aggiungi(whiteRect, 140)
-#     sk.aggiungi(whiteRect, 180)
-    sk.aggiungi(redRect, 30)
-    sk.aggiungi(redRect, 70)
-    sk.aggiungi(redRect, 110)
-    sk.aggiungi(redRect, 150)
-    sk.aggiungi(greenRect, 40)
-    sk.aggiungi(greenRect, 80)
-    sk.aggiungi(greenRect, 120)
-    sk.aggiungi(greenRect, 160)
-#     sk.aggiungi(blackRect, 20)
-#     sk.aggiungi(blackRect, 190)
-#     skRed = Skyline(white)
-#     skRed.aggiungi(redRect, 1)
-#     skRed.aggiungi(redRect, 50)
-#     skRed.aggiungi(redRect, 25)
-#     sk.fondi(skRed)
-    #print(red.utilizzo(sk));
-    
-    # sk.fondi(sk)
-    # greenRect.cancella();
-    # print(green.utilizzo(sk))
-     
-    sk.salva('testSkyline.png')
-     
