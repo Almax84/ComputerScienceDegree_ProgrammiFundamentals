e6fb868d4f6af9379f2b0269241d237171ed1ca0
Scrimieri Davide (Ibm Italia SpA) <xibm44@informatica.aci.it>
2018-12-11 16:14:41 +0100

    boh

--- Fondamenti_di_programmazione/esercizi_2018/ricorsione/permutazione.py
+++ Fondamenti_di_programmazione/esercizi_2018/ricorsione/permutazione.py
@@ -4,9 +4,10 @@
 1. countf(path) che preso in input il percorso path di un file o directory ritorna
     il numero di totale di
    file contenuti a qualsiasi livello nella directory (se è una directory). Esempi
->>> countf('Informatica')					ritorna 27
->>> countf('Informatica/Software')				ritorna 19
->>> countf('Informatica/Hardware/Architetture/cache.txt')	ritorna  1
+
+# >>> countf('Informatica')					ritorna 27
+# >>> countf('Informatica/Software')				ritorna 19
+# >>> countf('Informatica/Hardware/Architetture/cache.txt')	ritorna  1
 '''
 ################################################################################
 import os
@@ -100,16 +101,18 @@ def maxlev_mio(path):
 
 
 
-print("maxlev ",maxlev("Informatica_2"))
+
 class TestSecondoEsercizio(unittest.TestCase):
-    #def test_ritorno_0(self):
-        #self.assertEqual(maxlev("Informatica/Teoria"),0)
+    def test_ritorno_0(self):
+        self.assertEqual(maxlev("Informatica/Teoria"),0)
         
     def test_ritorno_7(self):
-        pass
-        #self.assertEqual(maxlev("Informatica"),7)
-        #print("maxlev ",maxlev("Informatica_2"))
-    
+        self.assertEqual(maxlev("Informatica"),7)
+    def test_ritorno_6(self):
+        self.assertEqual(maxlev("Informatica/Hardware"),6)
+    def test_ritorno_1(self):
+        self.assertEqual(maxlev("Informatica/Software/SistemiOperativi/Linux.txt"),1)
+
     
 if __name__ == "__main__":
     unittest.main()
@@ -124,6 +127,33 @@ if __name__ == "__main__":
 
 ################################################################################
 
+'''
+
+def permute_d(seq):
+    
+    if len(seq) == 1:
+        return [seq]
+    
+    
+    return_list = []
+    for i in range(len(seq)):
+        X = seq[i:i+1]
+        seq_remainder = seq[:i] + seq[i+1:]
+
+        sotto_permutazioni = permute_d(seq_remainder)
+
+        for arr in sotto_permutazioni:
+            # if type(arr) != list:
+            #     arr = [arr]
+
+            x_arr = X + arr
+            if x_arr not in return_list:
+                return_list.append(x_arr)
+
+    return return_list
+
+
+'''
 4. change(r, coins) ritorna il numero di combinazioni diverse di dare il resto r con i valori delle
    monete nella lista coins . Si assume che coins contenga sempre il valore 1 e che sia ordinata in senso
    crescente. 
@@ -136,6 +166,49 @@ Suggerimento: conviene contare le combinazioni "generandole" con i valori ordina
 (per evitare di contare due volte la stessa combinazione); generare tutte quelle che iniziano con il primo
 valore, poi quelle che iniziano con il secondo valore e così via.
 
+'''
+def change(r, coins):
+
+    if len(coins) == 1:
+        return coins[0]
+    coins.sort()
+
+    return_list = []
+    for i in range(len(coins)):
+        count = 0
+        X = coins[i]
+        while count < r:
+            count += X + change(r, [X])
+        if count == r:
+            return_list.append(1)
+        else:
+            pass
+
+
+
+    for i in range(len(coins)):
+        count = 0
+        X = coins[i]
+        seq_remainder = coins[:i] + coins[i + 1:]
+        while count < r:
+            count += X + change(r, seq_remainder)
+        if count == r:
+            return_list.append(1)
+        else:
+            return 0
+
+
+
+
+    return len(return_list)
+
+
+
+print("change ", change(8, [1,5]))
+
+
+
+'''
 ################################################################################
 
 5. node_fstree(root, path) ritorna il nodo dell'albero di radice tree (di tipo FSNode ) che ha il path
